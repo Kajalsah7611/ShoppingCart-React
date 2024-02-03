33fa2af5296dda8fef9b56abe803e08b85465cc2
kajalsah7611 <kajalsah7611@gmail.com>
2024-02-03 21:09:50 +0530

    text formatted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,8 +13,8 @@ export default function Footer(props) {
       >
         Reset
       </button>
-      <div className="col-8 bg-warning pt-2 text-light  d-flex justify-content-center ">
-        <h5>Total Amount: </h5>{" "}
+      <div className="col-8 bg-warning pt-2 d-flex justify-content-center ">
+        <h5><b>Total Amount: </b></h5>{" "}
         <span className="badge bg-dark ms-3 mb-2">₹{props.totalAmount}</span>
       </div>
       <button className="btn btn-primary col-2">Pay Now</button>
